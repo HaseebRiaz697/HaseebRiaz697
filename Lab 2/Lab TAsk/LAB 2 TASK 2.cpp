18e1d0019c975d54b283962337163a0a37788c1b
HaseebRiaz697 <haseebriaz7504@gmail.com>
2026-03-11 05:19:01 +0500

    Add files via upload

---
+++ Lab 2/Lab TAsk/LAB 2 TASK 2.cpp
@@ -0,0 +1,18 @@
+// Name: Haseeb Riaz
+// Department: Electrical Engineering (AI)
+// Registration No: BF25NWELE0697
+// Section: B
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+    	float pi; // FLAOT USE KIA HA DECIMAL VALUE K LIYE
+    	pi=3.14; // VALUE DECLARE
+    	cout<<"value of pi =\t"<<pi; // DISPLAY
+
+
+return 0;
+}

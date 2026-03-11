3fd3755d02c176545c73bc4a917a3773d5685dc2
HaseebRiaz697 <haseebriaz7504@gmail.com>
2026-03-11 05:19:45 +0500

    Add files via upload

---
+++ Lab 3/Lab Task/LAB 3 TASK 3.cpp
@@ -0,0 +1,24 @@
+// Name: Haseeb Riaz
+// Department: Electrical Engineering (AI)
+// Registration No: BF25NWELE0697
+// Section: B
+
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+int num1; // VARIABLE DECLERE KRNY K LYE
+
+cout<<"enter num\n";// DISPLAY K LYE 
+cin>>num1;  // USER JO INPUT DE GA 
+
+if(num1>0)//   CONDITION LAGANY K LYE 
+cout<<num1<<" is positive"<<endl;   // PRINT KRNY K LYE 
+
+if(num1<0)//   CONDITION LAGAANY K LYE 
+cout<<num1<<" is negative"<<endl;//   DISPLAY KARNY K LYE 
+return 0;
+}

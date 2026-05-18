fb2b347a1640a22edf8e64eb99793a752dc35d07
HaseebRiaz697 <haseebriaz7504@gmail.com>
2026-05-18 22:49:33 +0500

    Create a C++ greeting program
    Added a simple C++ program that greets the user.

---
+++ Lab 09/Lab Task/Task 1 .cpp
@@ -0,0 +1,21 @@
+// Name: Haseeb Riaz
+// Department: Electrical Engineering (AI)
+// Registration No: BF25NWELE0697
+// Section: B
+#include <iostream> // tells the compiler to include input output stream library
+using namespace std; // tells the compiler to use standard namespace std for each cin and cout
+
+void greet(){      // function declaration 
+	
+	cout<<" welcome to cpp ";}  // welcome meassage sent to display
+	
+int main (){  // main function beginns from here
+	
+	greet();  // function call
+	
+	return 0;  // tells the computer that program has ended successfully
+
+	
+}
+	
+	

49c31f95fa52169870390dacad676852b133b11e
HaseebRiaz697 <haseebriaz7504@gmail.com>
2026-05-18 22:50:40 +0500

    Create Lab Task 2.cpp

---
+++ Lab 09/Lab Task/Lab Task 2.cpp
@@ -0,0 +1,19 @@
+// Name: Haseeb Riaz
+// Department: Electrical Engineering (AI)
+// Registration No: BF25NWELE0697
+// Section: B
+#include <iostream> // tells the compiler to include input output stream library
+using namespace std; // tells the compiler to use standard namespace std for each cin and cout
+
+
+
+void sayhello(){    // function declaration 
+	cout<<" hello world "; // hello world message sent to display
+}
+
+int main() {  // main function beginns from here
+	
+	sayhello();  // function call
+	
+return 0;  // tells the computer that program has ended successfully
+}
